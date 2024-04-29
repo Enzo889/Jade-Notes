5505dda8868275b0ca960892362cf3d0ce062868
Enzo889 <enzobustamante895@gmail.com>
2024-04-29 19:49:34 -0300

    feat:add some components to the landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Heading } from "./_components/Heading";
 import { Heroes } from "./_components/Heroes";
+import { Footer } from "./_components/Footer";
 
 const marketingPage = () => {
   return (
@@ -8,6 +9,7 @@ const marketingPage = () => {
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
         <Heroes />
+        <Footer />
       </div>
     </div>
   );
